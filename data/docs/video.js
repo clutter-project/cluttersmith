7f536fd1408a84a054b9e8ffd39184960edcd3ed
Øyvind Kolås <pippin@linux.intel.com>
2009-10-27 17:16:42 +0000

    Updated json files with new properties after moving to mx

--- data/docs/video.js
+++ data/docs/video.js
@@ -1,20 +1,20 @@
-/*[CS] play:clicked*/ $("play").connect('clicked', function(o,event) {
-$("video").playing = true;
+/*[CS] go:clicked */  $('go').connect('clicked', function(o,event) {
+$("video").uri = $("uri").text;
 });/*[CS]*/
-/*[CS] pause:clicked*/ $("pause").connect('clicked', function(o,event) {
-$("video").playing = false;
-});/*[CS]*/
-/*[CS] rewind:clicked*/  $("rewind").connect('clicked', function(o,event) {
-$("video").progress = 0.0;
-});/*[CS]*/
-/*[CS] rew:clicked*/ $("rew").connect('clicked', function(o,event) {
-$("video").progress = $("video").progress - (10 / $("video").duration);
-});/*[CS]*/
-/*[CS] ffw:clicked*/ $("ffw").connect('clicked', function(o,event) {
+/*[CS] ffw:clicked */  $('ffw').connect('clicked', function(o,event) {
 $("video").progress = $("video").progress + (10 / $("video").duration);
 });/*[CS]*/
-/*[CS] go:clicked  */ $("go").connect('clicked', function(o,event) {
-$("video").uri = $("uri").text;
+/*[CS] rew:clicked */  $('rew').connect('clicked', function(o,event) {
+$("video").progress = $("video").progress - (10 / $("video").duration);
+});/*[CS]*/
+/*[CS] rewind:clicked */  $('rewind').connect('clicked', function(o,event) {
+$("video").progress = 0.0;
+});/*[CS]*/
+/*[CS] pause:clicked */  $('pause').connect('clicked', function(o,event) {
+$("video").playing = false;
+});/*[CS]*/
+/*[CS] play:clicked */  $('play').connect('clicked', function(o,event) {
+$("video").playing = true;
 });/*[CS]*/
 var a = 2;
 
