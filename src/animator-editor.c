50cf8cee524f440aaa0408244fc6f5c7d8406b57
Øyvind Kolås <pippin@linux.intel.com>
2010-02-23 16:51:59 +0000

    Add boilerplate for animator editor

---
+++ src/animator-editor.c
@@ -0,0 +1,145 @@
+#include "cluttersmith.h"
+#include <animator-editor.h>
+
+enum
+{
+  PROP_0,
+};
+
+struct _CsAnimatorEditorPrivate
+{
+  ClutterActor *background;
+};
+
+#define GET_PRIVATE(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), \
+                                                       CS_TYPE_ANIMATOR_EDITOR, \
+                                                       CsAnimatorEditorPrivate))
+G_DEFINE_TYPE (CsAnimatorEditor, cs_animator_editor, CLUTTER_TYPE_ACTOR)
+
+static void
+cs_animator_editor_finalize (GObject *object)
+{
+  G_OBJECT_CLASS (cs_animator_editor_parent_class)->finalize (object);
+}
+
+static void
+cs_animator_editor_dispose (GObject *object)
+{
+    CsAnimatorEditor *player = (CsAnimatorEditor *) object;
+    CsAnimatorEditorPrivate *priv;
+    priv = player->priv;
+
+    G_OBJECT_CLASS (cs_animator_editor_parent_class)->dispose (object);
+}
+
+static void
+cs_animator_editor_set_property (GObject      *object,
+                               guint         prop_id,
+                               const GValue *value,
+                               GParamSpec   *pspec)
+{
+    switch (prop_id) {
+    default:
+        break;
+    }
+}
+
+static void
+cs_animator_editor_get_property (GObject *object, guint prop_id,
+                               GValue     *value,
+                               GParamSpec *pspec)
+{
+  switch (prop_id)
+    {
+      default:
+        break;
+    }
+}
+
+static void
+cs_animator_editor_allocate (ClutterActor *actor, const ClutterActorBox *box,
+                           ClutterAllocationFlags flags)
+{
+  CsAnimatorEditor        *player = (CsAnimatorEditor *) actor;
+  CsAnimatorEditorPrivate *priv   = player->priv;
+
+  CLUTTER_ACTOR_CLASS (cs_animator_editor_parent_class)->allocate
+                             (actor, box, flags);
+
+  if (priv->background ) 
+      clutter_actor_allocate_preferred_size (priv->background,
+                                             flags);
+}
+
+static void
+cs_animator_editor_paint (ClutterActor *actor)
+{
+  CsAnimatorEditor        *player = (CsAnimatorEditor *) actor;
+  CsAnimatorEditorPrivate *priv   = player->priv;
+
+  if (priv->background)
+     clutter_actor_paint (priv->background);
+}
+
+static void
+cs_animator_editor_pick (ClutterActor *actor, const ClutterColor *color)
+{
+    cs_animator_editor_paint (actor);
+}
+
+static void
+cs_animator_editor_map (ClutterActor *self)
+{
+    CsAnimatorEditorPrivate *priv = CS_ANIMATOR_EDITOR (self)->priv;
+
+    CLUTTER_ACTOR_CLASS (cs_animator_editor_parent_class)->map (self);
+
+    if (priv->background)
+      clutter_actor_map (CLUTTER_ACTOR (priv->background));
+}
+
+static void
+cs_animator_editor_unmap (ClutterActor *self)
+{
+    CsAnimatorEditorPrivate *priv = CS_ANIMATOR_EDITOR (self)->priv;
+
+    CLUTTER_ACTOR_CLASS (cs_animator_editor_parent_class)->unmap (self);
+
+    if (priv->background)
+      clutter_actor_unmap (CLUTTER_ACTOR (priv->background));
+}
+
+
+static void
+cs_animator_editor_class_init (CsAnimatorEditorClass *klass)
+{
+    GObjectClass      *o_class = (GObjectClass *) klass;
+    ClutterActorClass *a_class = (ClutterActorClass *) klass;
+
+    o_class->dispose      = cs_animator_editor_dispose;
+    o_class->finalize     = cs_animator_editor_finalize;
+    o_class->set_property = cs_animator_editor_set_property;
+    o_class->get_property = cs_animator_editor_get_property;
+
+    a_class->allocate = cs_animator_editor_allocate;
+    a_class->paint    = cs_animator_editor_paint;
+    a_class->pick     = cs_animator_editor_pick;
+    a_class->map      = cs_animator_editor_map;
+    a_class->unmap    = cs_animator_editor_unmap;
+
+    g_type_class_add_private (klass, sizeof (CsAnimatorEditorPrivate));
+}
+
+static void
+cs_animator_editor_init (CsAnimatorEditor *self)
+{
+  CsAnimatorEditorPrivate *priv = GET_PRIVATE (self);
+
+  self->priv = priv;
+
+  priv->background = clutter_rectangle_new ();
+  clutter_actor_set_reactive (priv->background, TRUE);
+  clutter_actor_set_parent (priv->background, CLUTTER_ACTOR (self));
+  clutter_actor_show (priv->background);
+}
+
---
+++ src/animator-editor.h
@@ -0,0 +1,49 @@
+#ifndef __CS_ANIMATOR_EDITOR_H__
+#define __CS_ANIMATOR_EDITOR_H__
+
+#include <clutter/clutter.h>
+
+G_BEGIN_DECLS
+
+#define CS_TYPE_ANIMATOR_EDITOR    \
+  (cs_animator_editor_get_type ())
+#define CS_ANIMATOR_EDITOR(obj)    \
+  (G_TYPE_CHECK_INSTANCE_CAST ((obj),  \
+                               CS_TYPE_ANIMATOR_EDITOR, \
+                               CsAnimatorEditor))
+#define CS_ANIMATOR_EDITOR_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST ((klass),    \
+                            CS_TYPE_ANIMATOR_EDITOR, \
+                            CsAnimatorEditorClass))
+#define IS_CS_ANIMATOR_EDITOR(obj)    \
+  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), \
+                               CS_TYPE_ANIMATOR_EDITOR))
+#define IS_CS_ANIMATOR_EDITOR_CLASS(klass)  \
+  (G_TYPE_CHECK_CLASS_TYPE ((klass),    \
+                            CS_TYPE_ANIMATOR_EDITOR))
+#define CS_ANIMATOR_EDITOR_GET_CLASS(obj) \
+  (G_TYPE_INSTANCE_GET_CLASS ((obj),     \
+                              CS_TYPE_ANIMATOR_EDITOR, \
+                              CsAnimatorEditorClass))
+
+typedef struct _CsAnimatorEditorPrivate CsAnimatorEditorPrivate;
+typedef struct _CsAnimatorEditor CsAnimatorEditor;
+typedef struct _CsAnimatorEditorClass CsAnimatorEditorClass;
+
+struct _CsAnimatorEditor
+{
+  ClutterActor parent;
+
+  CsAnimatorEditorPrivate *priv;
+};
+
+struct _CsAnimatorEditorClass
+{
+  ClutterActorClass parent_class;
+};
+
+GType cs_animator_editor_get_type (void) G_GNUC_CONST;
+
+G_END_DECLS
+
+#endif /* __CS_ANIMATOR_EDITOR_H__ */
